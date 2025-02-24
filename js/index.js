bc454917f75b4fdbe55a4d081b345678fcd868a4
Diana Ortiz <diana.lopezofi16@gmai.com>
2025-02-23 21:35:17 -0600

    Se ha agregado menu de hamburguesa

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
-//menu hamburguesa
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
+const hamburguer = document.getElementById('hamburguer');
+const nav = document.querySelector('.nav');
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+hamburguer.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
